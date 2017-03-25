a2201ef3523f12317cc45be61dfc9bd694f65218
Ivan Khoteev <ivan.khoteev@saltpepper.ru>
2017-03-25 20:41:58 +0300

    Refactor code

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,24 +2,10 @@ const socket = window.io();
 
 socket.on('connect', () => {
   console.log('Successfully connected to the server');
-
-  socket.emit('sendEmail', {
-    to: 'wer@example.com',
-    text: 'text',
-  });
-
-  socket.emit('createMessage', {
-    from: 'some user',
-    text: 'text message',
-  });
 });
 
 socket.on('disconnect', () => {
   console.log('Connection failed');
-});
-
-socket.on('newEmail', email => {
-  console.log(`New email was coming\nfrom: ${email.from},\nwith text: ${email.text},\nat: ${new Date(email.createdAt)}`);
 });
 
 socket.on('newMessage', data => {
--- server/server.js
+++ server/server.js
@@ -17,16 +17,6 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
-  socket.emit('newEmail', {
-    from: 'ivan @example.com',
-    text: 'Email text',
-    createdAt: Date.now(),
-  });
-
-  socket.on('sendEmail', data => {
-    console.log(`Send email with data: ${JSON.stringify(data, undefined, 2)}`);
-  });
-
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
@@ -34,7 +24,7 @@ io.on('connection', socket => {
   socket.on('createMessage', data => {
     console.log('create a new message and seed them');
     console.log(JSON.stringify(data, undefined, 2));
-    socket.emit('newMessage', data);
+    io.emit('newMessage', data);
   });
 });
 
