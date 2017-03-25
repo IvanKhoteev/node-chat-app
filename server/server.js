2ae83e553558d8d10bd97338449948f4b948691c
Ivan Khoteev <ivan.khoteev@saltpepper.ru>
2017-03-25 21:00:32 +0300

    Add emit for all and for all except by myself

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  io.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome all',
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'For all except admin',
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
@@ -24,7 +34,8 @@ io.on('connection', socket => {
   socket.on('createMessage', data => {
     console.log('create a new message and seed them');
     console.log(JSON.stringify(data, undefined, 2));
-    io.emit('newMessage', data);
+    // io.emit('newMessage', data);
+    // socket.broadcast.emit('newMessage', data);
   });
 });
 
